bb9e8005dbe7b5e092150a4675d8bfaf52067e66
Saurish Ghosh <knotalun@gmail.com>
2025-03-15 18:01:28 +0000

    feat: messing around with some notifications

--- hivemind_beta/lib/main.dart
+++ hivemind_beta/lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hivemind_beta/firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 // import 'locator.dart';
+
+ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message ${message}");
+ } 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: false, 
+    criticalAlert: false,
+    provisional: false,
+    sound: true
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  final fcmToken = await FirebaseMessaging.instance.getToken(vapidKey: "BMODN9xVEPIcUOblSSaauM6JOPtso9pQdZltFZnOIz7eA5w6tICs6Q8GbhqO8VC22a0vddXC8_UN07aOWsxpwPQ");
+  FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) => { }).onError((err) => { });
+  // String? token = await FirebaseMessaging.instance.getToken();
+  print('User Granted Permission ${settings.authorizationStatus}');
   runApp(const MyApp());
 }
 
--- hivemind_beta/macos/Flutter/GeneratedPluginRegistrant.swift
+++ hivemind_beta/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,14 @@ import Foundation
 
 import firebase_core
 import firebase_database
+import firebase_messaging
+import flutter_local_notifications
 import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
